0a666f4e0ec4eb4097127702619e5f2fc77c7f03
Artiom Brantov <www.arem@mail.ru>
2023-09-08 15:53:46 +0700

    Create game Snake

--- wordsGame.js
+++ wordsGame.js
@@ -9,46 +9,66 @@
 // }
 
 // Создаем массив со словами
-let words = [
-    "программа",
-    "макака",
-    "прекрасный",
-    "оладушек"
-   ];
+// let words = [
+//     "программа",
+//     "макака",
+//     "прекрасный",
+//     "оладушек"
+//    ];
 
-// Выбираем случайное слово
-let word = words[Math.floor(Math.random() * words.length)];
+// // Выбираем случайное слово
+// let word = words[Math.floor(Math.random() * words.length)];
 
-// Создаем итоговый массив
-let answerArray = [];
+// // Создаем итоговый массив
+// let answerArray = [];
 
-for (let i = 0; i < word.length; i++) {
-    answerArray [i] = "_";
+// for (let i = 0; i < word.length; i++) {
+//     answerArray [i] = "_";
+// }
+
+// let remainingLetters = word.length;
+
+// //Игровой цикл
+// while (remainingLetters > 0) {
+//     // Показываем состояние игры
+//     alert(answerArray.join(" "));
+//     // Основной код
+//     let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
+//     if (guess === null) {
+//         break;
+//     }
+//     else if (guess !== word[1]) {
+//         alert("Хули вылупился как из яйца?");
+//     } 
+//     else {
+//         for (let j=0; j < word.length; j++) {
+//             if (word[j] === guess) {
+//                 answerArray[j] = guess;
+//                 remainingLetters--;
+//             }
+//         }
+//     }
+//     alert(answerArray.join(" "));
+//     alert("Отлично! Было загадано слово " + word);
+
+//    }
+
+//Упражнения после игры висилица
+
+//Создаем функцию, складывающую 2 числа
+    function add (x,y) {
+        z=x+y;
+        return z; 
+    }
+
+    add(12,15);
+    console.log(z);
+
+ //Создаем функцию, умножающую 2 числа
+ function multiply (x,y) {
+    z=x*y;
+    return z; 
 }
 
-let remainingLetters = word.length;
-
-//Игровой цикл
-while (remainingLetters > 0) {
-    // Показываем состояние игры
-    alert(answerArray.join(" "));
-    // Основной код
-    let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
-    if (guess === null) {
-        break;
-    }
-    else if (guess !== 1) {
-        alert("Хули вылупился как из яйца?");
-    } 
-    else {
-        for (let j=0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
-            }
-        }
-    }
-    alert(answerArray.join(" "));
-    alert("Отлично! Было загадано слово " + word);
-
-   }
+multiply(12,15);
+console.log(multiply(12,15)+add(12,15));
